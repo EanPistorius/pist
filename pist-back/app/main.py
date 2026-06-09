30875688dca477594aa3e880e41bd0ab5aece695
EanPistorius <ean.pistorius@gmail.com>
2026-06-09 06:55:53 +0000

    added router with api prefix to prevent 404s @/api/subscribe

--- pist-back/app/main.py
+++ pist-back/app/main.py
@@ -1,5 +1,5 @@
 #author: Ean Pistorius @ tomcatendeavours
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, APIRouter
 from sqlalchemy.orm import Session
 
 from app import SubscriberRequest, SessionLocal, Base, engine
@@ -13,11 +13,16 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://192.168.1.92:3000"
+        "http://192.168.1.92:3000",
+        "pistoriusies.co.za",
+        "www.pistoriusies.co.za",
+        "pistoriusies.co.za/troue",
+        "www.pistoriusies.co.za/troue"
     ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+router = APIRouter(prefix="/api")
 
 def get_db():
     db = SessionLocal()
@@ -26,7 +31,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/subscribe")
+@router.post("/subscribe")
 def subscribe(data: SubscriberRequest, db: Session = Depends(get_db)):
     
     subscriber = SubscriberService.subscribe(db,
@@ -38,7 +43,8 @@ def subscribe(data: SubscriberRequest, db: Session = Depends(get_db)):
 
     return {"status": "ok", "message": "RSVP suksesvol ontvang. / RSVP successfully received."}
 
-@app.get("/health")
+@router.get("/health")
 def health():
     return {"status": "ok"}
 
+app.include_router(router)
